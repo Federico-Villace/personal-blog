c55bc63131ca4cd0de6f33748ea848d46a00553e
Federico Villace <federicovillace@gmail.com>
2023-01-12 12:48:09 -0300

    updated home page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,14 +3,14 @@ import Button from "../Button";
 
 const Header = (): JSX.Element => {
   return (
-    <div className="flex items-center w-full h-[120px] bg-yellow ">
+    <div className="flex items-center w-full h-[120px] bg-light-yellow border-1 border-black border-b-black ">
       <div className="flex flex-wrap items-center w-full mx-[30px] justify-between capitalize">
         <div>
           <p className="text-[60px] font-light text-purple drop-shadow-lg">
             FNV
           </p>
         </div>
-        <div className="flex gap-8">
+        <div className="flex gap-8 font-Inconsolata">
           <Button variant={"primary"} text={"Home"} />
           <Button variant={"secondary"} text={"Proyects"} />
           <Button variant={"secondary"} text={"About Me"} />
--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -5,10 +5,18 @@ const Landing = (): JSX.Element => {
   return (
     <div>
       <Header />
-      <div className="flex items-center w-full h-[420px] bg-light-yellow ">
-        <div className="flex flex-wrap items-center w-full mx-[30px] justify-around ">
-          <div> fotito </div>
-          <div> texto </div>
+      <div className="flex items-center w-full h-[420px] bg-light-yellow font-Inconsolata">
+        <div className="flex flex-col items-center w-full justify-center">
+          <div className="flex flex-col items-center p-5 font-normal text-4xl">
+            <h1>Hi!, I'm Federico Villace</h1>
+            <h2 className="text-purple p-2">Full Stack Developer</h2>
+          </div>
+          <div className="flex flex-col max-w-xl items-center text-center font-normal text-lg">
+            <p>
+              + 2 years of experience working with innovative products and
+              creating high end solutions
+            </p>
+          </div>
         </div>
       </div>
     </div>
